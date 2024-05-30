db83263df96a2e138947af2eb69c96c44668c5c8
Shital Ingle <126812171+shitu-ingle@users.noreply.github.com>
2024-05-30 15:01:35 +0530

    Add files via upload

---
+++ BikeDriver.java
@@ -0,0 +1,16 @@
+// Single inheritance
+
+package inheritance;
+
+public class BikeDriver {
+
+	public static void main(String[] args) {
+	RX100 r1=new RX100(2, 2, "Petrol","YAMAHA","Red", 150000,34);
+	r1.displayBike();
+	System.out.println("====================================");
+	
+	r1.displayRX100();
+
+	}
+
+}
